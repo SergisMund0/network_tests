9a734c8d3ad9c45430eb5b302c071d62da9071fb
SergisMund0 <SergisMund0@users.noreply.github.com>
2019-06-15 22:01:47 +0400

    Add check for query parameters

--- lib/network_layer/http_request.dart
+++ lib/network_layer/http_request.dart
@@ -31,7 +31,7 @@ abstract class HttpRequestProtocol {
   /// Do not override this getter. Whenever the method's request is GET,
   /// the layer will concatenate the parameters into the query.
   String get queryParameters {
-    if (method == HttpMethod.GET) {
+    if (method == HttpMethod.GET && parameters != null) {
       final jsonString = Uri(queryParameters: parameters);
       return '?${jsonString.query}';
     }
